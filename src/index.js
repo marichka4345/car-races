80c72f2b82d69fa4880331e5a4971ab959473400
Mariia Tretiak <marichka4345@gmail.com>
2018-10-09 17:16:48 +0300

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from './main.scss';
+
+ReactDOM.render(
+  <div className={styles.title}>Hello!</div>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPluginConfig = new ExtractTextPlugin({
+  filename: 'main.css',
+  allChunks: true
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+    publicPath: '/'
+  },
+  resolve: { extensions: ['.js'] },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1
+              }
+            },
+            'sass-loader'
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig, ExtractTextPluginConfig]
+};
